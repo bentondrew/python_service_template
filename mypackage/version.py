be738b3d93b8c497856da0f7803e56a7f46603e1
Benton Drew <bentondrew24@gmail.com>
2022-06-20 19:36:25 -0500

    move version stuff to own module.

---
+++ mypackage/version.py
@@ -0,0 +1,13 @@
+from os import environ
+
+
+def set_version():
+    """
+    Read the PYTHON_PACKAGE_VERSION from the environment variables. If that variable is not
+    defined, return a default value of '1.0.0+pkgdefault'.
+    """
+    version = environ.get("PYTHON_PACKAGE_VERSION")
+    return version if version is not None else "1.0.0+pkgdefault"
+
+
+__version__ = set_version()
